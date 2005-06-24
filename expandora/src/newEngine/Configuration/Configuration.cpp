4964cf739fa36f5d259d2d6dbda634ef434e4350
alve <>
2005-06-24 07:09:10 +0000

    configuration improvements

--- expandora/src/newEngine/Configuration/Configuration.cpp
+++ expandora/src/newEngine/Configuration/Configuration.cpp
@@ -1,6 +1,7 @@
 #include "Configuration.h"
 #include <iostream>
 #include <qvariant.h>
+#include <qapplication.h>
 
 /**
  * this method is called when a component of this type should be
@@ -8,96 +9,110 @@
  * and handles platform specific issues
  */
 extern "C" MY_EXPORT Component * createComponent() {
-  return new Configuration;
+	return new Configuration;
 }
 
-Configuration::Configuration()  : currentComponent(this) {
-  put(*(new QString("configuration")), this);
+Configuration::Configuration() : currentComponent( this ) {
+	put( *( new QString( "configuration" ) ), this );
 }
 
-void Configuration::start(QThread::Priority) {
-  QFile xmlFile( property("fileName").toString());
-  QXmlInputSource source( &xmlFile );
+void Configuration::start( QThread::Priority ) {
+	QFile xmlFile( property( "fileName" ).toString() );
+	QXmlInputSource source( &xmlFile );
 
-  QXmlSimpleReader reader;
-  reader.setContentHandler(this);
-  reader.parse( source );
+	QXmlSimpleReader reader;
+	reader.setContentHandler( this );
+	reader.parse( source );
 }
 
-Component * Configuration::get(QString & id) {
-  return (*components.find(id)).second;
-}
-
-
-void Configuration::put(QString & id, Component * component) {
-  pair<QString , Component *> val;
-  val.first = id;
-  val.second = component;
-  components.insert(val);
+Component * Configuration::get
+	( QString & id ) {
+	return ( *components.find( id ) ).second;
 }
 
 
-
-bool Configuration::startElement( const QString& , const QString& , 
-				  const QString& qName, 
-				  const QXmlAttributes& attributes)
-{
-  if (qName == "component") newComponent(attributes);
-  else if (qName == "option") addOption(attributes);
-  else if (qName == "connection") connectComponents(attributes);
-  else if (qName == "configuration") return true;
-  else return false;
-  return true;
+void Configuration::put( QString & id, Component * component ) {
+	pair<QString , Component *> val;
+	val.first = id;
+	val.second = component;
+	components.insert( val );
 }
 
-bool Configuration::endElement ( const QString &, const QString &, 
-				      const QString & qName ) 
-{
-  if (qName == "component") currentComponent->start();  
-  return true;
+
+
+bool Configuration::startElement( const QString& , const QString& ,
+                                  const QString& qName,
+                                  const QXmlAttributes& attributes ) {
+	if ( qName == "component" )
+		newComponent( attributes );
+	else if ( qName == "option" )
+		addOption( attributes );
+	else if ( qName == "connection" )
+		connectComponents( attributes );
+	else if ( qName == "configuration" )
+		setPath( attributes );
+	else
+		return false;
+	return true;
 }
 
-void Configuration::newComponent(const QXmlAttributes & atts) {
-  QString id = atts.value("id");
-  QString file = atts.value("file");
-  QLibrary * lib;
-  
-  if (libs.find(file) == libs.end()) {
-    lib = new QLibrary(file);
-    libs.insert(make_pair(file, lib));
-  }
-  else lib = (*libs.find(file)).second;
-  
-  componentCreator creator = (componentCreator) lib->resolve("createComponent");
-	if (creator == 0) {
-		if (lib->isLoaded()) cout << "library loaded but creator not found: " << lib->library() << "\n";
-		else cout << "library can't be loaded: " << lib->library() << "\n";
-		return;
+void Configuration::setPath( const QXmlAttributes & atts ) {
+	QString path = atts.value( "path" );
+	qApp->addLibraryPath( path );
+}
+
+bool Configuration::endElement ( const QString &, const QString &,
+                                 const QString & qName ) {
+	if ( qName == "component" )
+		currentComponent->start();
+	return true;
+}
+
+void Configuration::newComponent( const QXmlAttributes & atts ) {
+	QString id = atts.value( "id" );
+	QString file = atts.value( "file" );
+	QLibrary * lib;
+
+	if ( libs.find( file ) == libs.end() ) {
+		lib = new QLibrary( file );
+		libs.insert( make_pair( file, lib ) );
+	} else
+		lib = ( *libs.find( file ) ).second;
+
+	componentCreator creator = ( componentCreator ) lib->resolve( "createComponent" );
+	if ( creator == 0 ) {
+		if ( lib->isLoaded() )
+			cout << "library loaded but creator not found: " << lib->library() << "\n";
+		else
+			cout << "library can't be loaded: " << lib->library() << "\n";
+		exit(-1) ;
 	}
-  currentComponent = creator();
-  put(id, currentComponent);
+	currentComponent = creator();
+	put( id, currentComponent );
 }
 
-void Configuration::addOption(const QXmlAttributes & atts) {
-  QString  name = atts.value("name");
-  QString  value = atts.value("value");
-  currentComponent->setProperty((const char*)name, value);
+void Configuration::addOption( const QXmlAttributes & atts ) {
+	QString name = atts.value( "name" );
+	QString value = atts.value( "value" );
+	currentComponent->setProperty( ( const char* ) name, value );
 }
 
-void Configuration::connectComponents(const QXmlAttributes & atts) {
-  QString from = atts.value("from");
-  QString sig = atts.value("signal");
-  QString to = atts.value("to");
-  QString sl = atts.value("slot");
-  
-  
-get(to)->connect(get(from), signal(sig), slot(sl));
+void Configuration::connectComponents( const QXmlAttributes & atts ) {
+	QString from = atts.value( "from" );
+	QString sig = atts.value( "signal" );
+	QString to = atts.value( "to" );
+	QString sl = atts.value( "slot" );
+
+
+	get
+		( to ) ->connect( get
+			                  ( from ), signal( sig ), slot( sl ) );
 }
 
-QString & Configuration::signal(QString & name) {
-  return name.prepend(QSIGNAL_CODE); 
+QString & Configuration::signal( QString & name ) {
+	return name.prepend( QSIGNAL_CODE );
 }
 
-QString & Configuration::slot(QString & name) {
-  return name.prepend(QSLOT_CODE); 
+QString & Configuration::slot( QString & name ) {
+	return name.prepend( QSLOT_CODE );
 }
